2458a23471de8bb67f02a7adc219f04c1cc579ac
a.bubelev <bubeleu@gmail.com>
2022-03-27 16:38:46 +0300

    star

--- src/Menu.js
+++ src/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Recipe from "./Recipe";
+import Stars from "./Star";
 
 
 
@@ -7,6 +8,7 @@ function Menu(props) {
     return (
       <>
         <h1>{props.title}</h1>
+        <Stars/>
         <div>
           {props.data.map((item, i) => (
             <Recipe key={i} recipe={item} />))
