931a3f5bd6aad0f0d20f670cbe3bdd1e5e6d79bd
a.bubelev <bubeleu@gmail.com>
2022-03-27 17:33:22 +0300

    star rating

--- src/Star.js
+++ src/Star.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 
 
 
-const Stars = ({ selected = false }) => {
+const Stars = ({ selected = false, onSelect = f => f }) => {
+    console.log(onSelect)
     return (
-        <FaStar color={selected ? 'red' : 'grey'} />
+        <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />
     )
 }
 
 const createArr = length => [...Array(length)];
 
 function StarRating({ total = 5 }) {
-    return createArr(total).map((n, i) => <Stars key={i} />)
+    const [selectedStars, setSelectStars] = useState(0);
 
+    return (
+        <>
+            {createArr(total).map((n, i) => (
+                <Stars
+                    key={i}
+                    selected={selectedStars > i}
+                    onSelect={() => setSelectStars(i + 1)} />
+            ))}
+            <div>{selectedStars} of {total} stars</div>
+        </>
+    )
 }
 
 export default StarRating
